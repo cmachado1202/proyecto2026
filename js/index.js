301fcdd2bf92dd0fc79e06dda3dcf4c50bf88aa6
cmachado1202 <carohandball@hotmail.com>
2026-07-01 11:43:13 -0300

    Actualizo precios

--- js/index.js
+++ js/index.js
@@ -11,47 +11,47 @@ const URL_PRODUCTOS = "https://dummyjson.com/products?limit=9";
 const alfajores = [
   {
     nombre: "Nuevo Alfajor GOAT",
-    precio: 5000,
+    precio: 3000,
     img: "./img/goat.jpg",
   },
   {
     nombre: "Alfajor Triple Bon o Bon Blanco",
-    precio: 5000,
+    precio: 1200,
     img: "./img/bonobont.jpg",
   },
   {
     nombre: "Alfajor Triple Mantecol",
-    precio: 5000,
+    precio: 1300,
     img: "./img/mantecolt.jpg",
   },
   {
     nombre: "Alfajor Triple Rasta Trico",
-    precio: 5000,
+    precio: 2500,
     img: "./img/trico.jpg",
   },
   {
     nombre: "Alfajor Triple Red Velvet",
-    precio: 5000,
+    precio: 1900,
     img: "./img/redbelvet.jpg",
   },
   {
     nombre: "Alfajor Triple Shot Chocolate Blanco",
-    precio: 5000,
+    precio: 1600,
     img: "./img/trishot.jpg",
   },
   {
     nombre: "Alfajor Simple Pescado Raúl",
-    precio: 5000,
+    precio: 1400,
     img: "./img/pescadoraul.jpg",
   },
   {
     nombre: "Alfajor Terrabusi",
-    precio: 5000,
+    precio: 1000,
     img: "./img/terrabusi.jpg",
   },
   {
     nombre: "Alfajor Milka Dulce de Leche Blanco",
-    precio: 5000,
+    precio: 1200,
     img: "./img/milkasdlblanco.jpg",
   },
 ];
